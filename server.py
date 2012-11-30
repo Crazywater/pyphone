61667445a6c64706669d4305684269976ca97854
Crazywater <crazytofire@web.de>
2012-11-30 17:53:26 +0100

    removing unused variables

--- server.py
+++ server.py
@@ -5,12 +5,9 @@ from speaker import Speaker
 import socket
 
 class Server():
-  chunksize = 1024
-  
   def __init__(self):
     self.netToAudio = NetToAudio()
     self.speaker = Speaker()
-    self.connected = False
     
   def listen(self):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,7 +19,6 @@ class Server():
   
   def recvFrom(self, socket):
     print "Listening on port {0}".format(Config.port)
-
     while True:
       chunk = self.readChunk(socket)
       audio = self.netToAudio.convert(chunk)
