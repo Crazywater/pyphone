253bb14d3849376e15e31ce401f8b5b5bd6989f2
Crazywater <crazytofire@web.de>
2012-12-09 14:26:25 +0100

    Add a counter (Server side)

--- nettoaudio.py
+++ nettoaudio.py
@@ -9,7 +9,7 @@ class NetToAudio():
     self.speex = speex.new(Config.speex_quality)
   
   def convert(self, netinput):
-    #return netinput
-    ints = netinput
-    ints = self.speex.decode(netinput)
+    counter, payload = struct.unpack("!Ls", netinput)
+    print "Received packet {0}".format(counter)
+    ints = self.speex.decode(payload)
     return struct.pack(len(ints)*Config.speex_fmt, *ints)
