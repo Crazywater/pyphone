3e86ed5d70a65b6e32dfed547c794990d8da5a90
Crazywater <crazytofire@web.de>
2012-12-09 16:08:58 +0100

    bugfixes

--- nettoaudio.py
+++ nettoaudio.py
@@ -9,8 +9,8 @@ class NetToAudio():
     self.speex = speex.new(Config.speex_quality)
   
   def convert(self, netinput):
-    counter = struct.unpack("!L", netinput[:8])
-    payload = netinput[8:]
-    print "Received packet {0} payloadsize {1}".format(counter, len(payload))
+    counter = struct.unpack("!L", netinput[:4])
+    payload = netinput[4:]
+    print "Received packet {0} payloadsize {1}".format(counter[0], len(payload))
     ints = self.speex.decode(payload)
     return struct.pack(len(ints)*Config.speex_fmt, *ints)
